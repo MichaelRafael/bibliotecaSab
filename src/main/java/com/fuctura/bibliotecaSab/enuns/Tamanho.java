7a5346a232ae5d2e7592ae01f4a9488acf4a4bff
MichaelRafael <michael-ras@hotmail.com>
2024-06-01 12:37:17 -0300

    primeiro commit

---
+++ src/main/java/com/fuctura/bibliotecaSab/enuns/Tamanho.java
@@ -0,0 +1,14 @@
+package com.fuctura.bibliotecaSab.enuns;
+
+public enum Tamanho {
+
+    PEQUENO(0),
+    MEDIO(1),
+    GRANDE(2);
+
+    private Integer cod;
+
+    Tamanho(Integer cod) {
+        this.cod = cod;
+    }
+}
